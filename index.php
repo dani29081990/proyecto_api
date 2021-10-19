fe698b82ce5fcfb3bd3069d360b519d02b77ba9d
Javier Catalán <javier.catalan7@gmail.com>
2021-10-19 17:00:14 +0200

    unir la aplicación

--- index.php
+++ index.php
@@ -2,5 +2,33 @@
 $data = json_decode( file_get_contents('https://dog.ceo/api/breeds/image/random'), true );
 
 $image= $data['message'];
+$mostrar = '<img src ='.$image.' style="width:20%">';
 
-echo "<img src ='$image'>";
+echo $mostrar;
+
+/* function mostrarimagen(){
+    echo "<img src ='$image'>";
+}
+$mostrarimaagen = mostrarimagen(); */
+//echo "<button type='submit'name='ok'onclick=$mostrarimaagen>Mostrarperro</button>";
+
+/* <button type="submit" name="ok" onclick="mostrarimagen()">Guardar</button>
+ */
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form class="form" method="POST" action="index.php"> 
+<button type="submit" name="ok" >MostrarPerro</button>
+</form>
+</body>
+</html>
