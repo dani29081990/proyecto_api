13bfea3e2b92f1772b57158a4177702e729d920a
javier <javier.chamorro@escuelaestech.es>
2021-10-19 17:03:05 +0200

    update de login

--- login.php
+++ login.php
@@ -4,6 +4,12 @@ $password=$_POST["password"];
 
 if(strcmp($nombre,"dani")==0 && strcmp($password,"dani")==0){
     header('Location: index.php');
+}elseif(strcmp($nombre,"javic")==0 && strcmp($password,"javic")==0){
+    header('Location: index.php');
+}elseif(strcmp($nombre,"javicc")==0 && strcmp($password,"javicc")==0){
+    header('Location: index.php');
+}else{
+    echo'ERROR EN INICIAR SESION';
 }
 
 
